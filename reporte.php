ccb9876e42c76d986b5cf2b3fc1c26f36846fd5b
Oliweb19 <oaguillen.24@est.ucab.edu.ve>
2025-09-11 16:13:58 -0400

    Se cambio a php para poder trabajar con la base de datos

---
+++ reporte.php
@@ -0,0 +1,90 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>VTV NetReport</title>
+    <link rel="stylesheet" href="css/style.css">
+    <link rel="icon"  href="img/logo_vtv.webp">
+    <link href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined" rel="stylesheet" />
+</head>
+<body>
+    
+    <div class="main">
+        <div class="menu">
+
+            <div class="rep_logo">
+                <div class="menu_logo">
+                    <img src="img/logo_vtv.webp" alt="" sizes="" srcset="">
+                </div>
+                <h1>NetReport</h1>
+            </div>
+
+            <div class="list_op">
+                <div class="op">
+                    <h4>
+                        <a href="#"> 
+                            <span class="material-symbols-outlined">dns</span>
+                            Switches
+                        </a>
+                    </h4>
+                </div>
+                <div class="op">
+                    <h4>
+                        <a href="#"> 
+                            <span class="material-symbols-outlined">host</span>
+                            Servidores
+                        </a>
+                    </h4>
+                </div>
+                <div class="op exit">
+                    <h4>
+                        <a href="#"> 
+                            <span class="material-symbols-outlined">arrow_back</span>
+                            Salir
+                        </a>
+                    </h4>
+                </div>
+
+            </div>
+            
+        </div>
+
+        <div class="cont-padre">
+            <div class="banner-name">
+                <h1>Buenas sr.XXX</h1>
+            </div>
+
+            <div class="cont-info">
+                <p>
+                    Lorem ipsum, dolor sit amet consectetur adipisicing elit. Nemo voluptatum inventore in numquam ducimus unde tempore ut consequuntur ratione molestias 
+                    quaerat, maxime fuga aliquid! Id accusamus nemo optio aspernatur dolores?
+                    Nisi ducimus esse dolorum repudiandae quia accusantium optio sapiente obcaecati vel veniam suscipit, ipsa officiis quasi? Sapiente suscipit iure officia 
+                    modi ipsam perferendis illum non eum! Consequatur dignissimos at unde.
+                    Neque autem voluptatum saepe sed modi esse numquam, commodi ratione consequuntur. Blanditiis porro voluptas similique ex doloribus. Quae recusandae 
+                    veritatis officiis incidunt, ducimus earum dignissimos repudiandae voluptatem minus eos commodi!
+                    Voluptates illum eveniet error cum laborum explicabo dolorem, quisquam dolore fugit! Temporibus, deserunt quibusdam similique nemo quos, 
+                    veritatis aliquam obcaecati consectetur sapiente dolorem eveniet, autem neque ipsam nisi iusto quo?
+                    Minima impedit dolorum commodi neque? Sit reprehenderit neque eaque excepturi magnam repellat dolore, error minima. Exercitationem enim 
+                    cumque ipsam amet aperiam architecto asperiores? Iste at doloribus architecto consequuntur! Voluptas, quasi.
+                    Saepe optio quidem quasi ipsum animi pariatur itaque quos, vero corporis alias tempore adipisci, sunt facere? Voluptas autem temporibus harum 
+                    
+                    dignissimos, enim dolor. Nobis earum perferendis quos quae ex animi!
+                    Ullam delectus cum, est quibusdam, fugit mollitia quam repellendus perferendis unde facilis corrupti laboriosam reiciendis quis hic veniam quae? 
+                    Eos saepe blanditiis numquam? Ducimus soluta illum possimus nisi dolor quasi.
+                    Odit ex praesentium possimus et quibusdam provident vero eaque tempora, nostrum dolorem quo quae quaerat, iste in numquam atque repudiandae est 
+                    optio quasi, eligendi quam dolore natus suscipit! Saepe, provident?
+                    Fugiat, cumque tempore! Consequuntur cupiditate obcaecati atque optio facere. Modi error aliquid aspernatur blanditiis harum quaerat reiciendis 
+                    
+                    qui fugit ipsa, quae architecto tenetur tempora, alias minus asperiores suscipit vero. Velit.
+                    Voluptatum ipsam reiciendis deleniti optio sed fugiat numquam quas tempora reprehenderit voluptatibus harum consectetur facilis necessitatibus, 
+                    provident, cupiditate exercitationem. At, explicabo vel illo aperiam totam nesciunt maiores hic adipisci vero?
+                </p>
+            </div>
+
+            <a href="#" class="btn-pdf"> Generar en PDF </a> 
+        </div>
+    </div>
+    
+</body>
+</html>
